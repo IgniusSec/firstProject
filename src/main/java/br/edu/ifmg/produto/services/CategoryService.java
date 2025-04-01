dcc80a186a02a4efd4919e333e214726bf249ffd
Ig <.>
2025-04-01 15:12:38 -0300

    Create function findById

--- src/main/java/br/edu/ifmg/produto/services/CategoryService.java
+++ src/main/java/br/edu/ifmg/produto/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,5 +20,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(categoria-> new CategoryDTO(categoria)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category category = obj.get();
+        return new CategoryDTO(category);
     }
 }
