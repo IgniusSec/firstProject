9a0e2fea51f5177307ae825f183573f09b552a7d
Ignius <ivansaveif@gmail.com>
2025-05-15 17:42:50 -0300

    Aula 13 - CRUD usuario

---
+++ src/main/java/br/edu/ifmg/produto/dtos/UserInsertDTO.java
@@ -0,0 +1,21 @@
+package br.edu.ifmg.produto.dtos;
+
+public class UserInsertDTO extends UserDTO{
+    private String password;
+
+    public UserInsertDTO() {
+        super();
+    }
+
+    public UserInsertDTO(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
