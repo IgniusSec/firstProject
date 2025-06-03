a2735752884241f39288345bfe14c350a89b9990
Ignius <ivansaveif@gmail.com>
2025-06-03 15:25:45 -0300

    Aula 18 - finalizando o endpoint paginado com filtro.

--- src/main/java/br/edu/ifmg/produto/projections/ProductProjection.java
+++ src/main/java/br/edu/ifmg/produto/projections/ProductProjection.java
@@ -3,6 +3,6 @@ package br.edu.ifmg.produto.projections;
 public interface ProductProjection {
     public Long getId();
     public String getName();
-    public String getImageURL();
-    public Double gerPrice();
+    public String getImageUrl();
+    public Double getPrice();
 }
